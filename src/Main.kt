6ae1f7e98e87f5f4ad802f8fdbf8419b7925a9a1
alex <alehin.av@edu.spbstu.ru>
2019-11-19 11:19:29 +0300

    Common case solver - Решает идеальнае случаи - Не протестирован на "сложные" ситуации

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,10 @@
 import entities.HalfPlain
+import entities.Line
 import entities.Point
 import entities.Polygon
 import java.io.File
 import kotlin.math.PI
+import kotlin.math.abs
 import kotlin.math.min
 
 //Common
@@ -46,16 +48,16 @@ fun findIntersection(list: MutableList<HalfPlain>): Polygon? = when (list.size) 
             checkIsRight(angle1) && checkIsLeft(angle2) -> Polygon(mutableListOf(list[1]), mutableListOf(list[0]))
             checkIsLeft(angle1) && checkIsLeft(angle2) -> {
                 if (angle1 <= angle2) {
-                    Polygon(mutableListOf(list[0], list[1]), mutableListOf())
-                } else {
                     Polygon(mutableListOf(list[1], list[0]), mutableListOf())
+                } else {
+                    Polygon(mutableListOf(list[0], list[1]), mutableListOf())
                 }
             }
             checkIsRight(angle1) && checkIsRight(angle2) -> {
                 if (angle1 <= angle2) {
-                    Polygon(mutableListOf(), mutableListOf(list[1], list[0]))
-                } else {
                     Polygon(mutableListOf(), mutableListOf(list[0], list[1]))
+                } else {
+                    Polygon(mutableListOf(), mutableListOf(list[1], list[0]))
                 }
             }
             else -> TODO()
@@ -74,7 +76,7 @@ fun findIntersectionOfPolygons(polygon1: Polygon?, polygon2: Polygon?) = when {
         when {
             polygon1!!.leftEdges.size + polygon1.rightEdges.size + polygon2!!.leftEdges.size + polygon2.rightEdges.size <= 4 -> {
                 leftEdges.addAll((polygon1.leftEdges + polygon2.leftEdges).sortedByDescending { it.getPolarAngle() })
-                rightEdges.addAll((polygon1.rightEdges + polygon2.rightEdges).sortedByDescending { it.getPolarAngle() })
+                rightEdges.addAll((polygon1.rightEdges + polygon2.rightEdges).sortedBy { it.getPolarAngle() })
             }
             polygon1.leftEdges.isEmpty() && polygon1.rightEdges.isNotEmpty() && polygon2.leftEdges.isNotEmpty() && polygon2.rightEdges.isEmpty() ||
                     polygon1.leftEdges.isNotEmpty() && polygon1.rightEdges.isEmpty() && polygon2.leftEdges.isEmpty() && polygon2.rightEdges.isNotEmpty() -> {
@@ -116,12 +118,12 @@ fun findIntersectionOfPolygons(polygon1: Polygon?, polygon2: Polygon?) = when {
 
                 // Find top - event point and event line (half-plain)
                 var lastEventPoint = Point(0.0, 0.0)
-                var topEventPointY = min(polygon1.findTop().y, polygon2.findTop().y)
-                //            var currentEventHalfPlain = HalfPlain(Line(0.0, -1.0, currentEventPoint!!.y))
+                val topEventPointY = min(polygon1.findTop().y, polygon2.findTop().y)
                 val endEventPointY = min(polygon1.findBottom().y, polygon2.findBottom().y)
-
-                var eventPointQueue = mutableListOf<Point>().apply {
-                    addAll(listOf(polygon1.findTop(), polygon2.findTop()).filter { it.y != Double.POSITIVE_INFINITY })
+                val eventPointQueue = mutableListOf<Point>().apply {
+                    addAll(listOf(polygon1.findTop(), polygon2.findTop()).filter {
+                        it.y != Double.POSITIVE_INFINITY && abs(it.y - topEventPointY) <= EPSILON
+                    })
                 }
 
                 // Traverse event points
@@ -131,97 +133,108 @@ fun findIntersectionOfPolygons(polygon1: Polygon?, polygon2: Polygon?) = when {
                     leftEdgeP2 = polygon2.leftEdges.getOrNull(leftEdgeP2Index)
                     rightEdgeP2 = polygon2.rightEdges.getOrNull(rightEdgeP2Index)
 
-                    if (eventPointQueue.isEmpty()) {
+                    for (eventPoint in eventPointQueue) {
+                        val currentEventHalfPlain = HalfPlain(Line(0.0, -1.0, eventPoint.y))
                         val edgesToHandle = mutableListOf<HalfPlain>().apply {
-                            leftEdgeP1?.let { add(it) }
-                            rightEdgeP1?.let { add(it) }
-                            leftEdgeP2?.let { add(it) }
-                            rightEdgeP2?.let { add(it) }
-                        }.sortedByDescending { it.getPolarAngle() }
-                        leftEdgeP1Top = leftEdgeP1?.let { polygon1.findTopCornerOfLeftEdge(leftEdgeP1, leftEdgeP1Index) }
-                        rightEdgeP1Top = rightEdgeP1?.let { polygon1.findTopCornerOfRightEdge(rightEdgeP1, rightEdgeP1Index) }
-                        leftEdgeP2Top = leftEdgeP2?.let { polygon2.findTopCornerOfLeftEdge(leftEdgeP2, leftEdgeP2Index) }
-                        rightEdgeP2Top = rightEdgeP2?.let { polygon2.findTopCornerOfRightEdge(rightEdgeP2, rightEdgeP2Index) }
+                            leftEdgeP1?.let { if (it.line.isPointInside(eventPoint)) add(it) }
+                            rightEdgeP1?.let { if (it.line.isPointInside(eventPoint)) add(it) }
+                            leftEdgeP2?.let { if (it.line.isPointInside(eventPoint)) add(it) }
+                            rightEdgeP2?.let { if (it.line.isPointInside(eventPoint)) add(it) }
+                        }
                         edgesToHandle.forEach { currentHalfPlain ->
+                            leftEdgeP1Top = leftEdgeP1?.let { polygon1.findTopCornerOfLeftEdge(leftEdgeP1, leftEdgeP1Index) }
+                            rightEdgeP1Top = rightEdgeP1?.let { polygon1.findTopCornerOfRightEdge(rightEdgeP1, rightEdgeP1Index) }
+                            leftEdgeP2Top = leftEdgeP2?.let { polygon2.findTopCornerOfLeftEdge(leftEdgeP2, leftEdgeP2Index) }
+                            rightEdgeP2Top = rightEdgeP2?.let { polygon2.findTopCornerOfRightEdge(rightEdgeP2, rightEdgeP2Index) }
                             when (currentHalfPlain) {
                                 // Working with Left of C1
                                 leftEdgeP1 -> leftEdgeP1.also {
-                                    when {
-                                        isPointInsideP2(leftEdgeP1Top!!, leftEdgeP2, rightEdgeP2) -> leftEdges.add(it)
-                                        rightEdgeP2?.isIntersectedBy(it) != null -> {
-                                            if (!rightEdgeP2.isPointInside(leftEdgeP1Top)) {
-                                                leftEdges.add(it)
-                                                rightEdges.add(rightEdgeP2)
-                                            }
+                                    if (isPointInsideP2(leftEdgeP1Top!!, leftEdgeP2, rightEdgeP2)) leftEdges.add(it)
+                                    if (rightEdgeP2?.isIntersectedBy(it) != null && !rightEdgeP2.isPointInside(leftEdgeP1Top!!)) {
+                                        val intersection = rightEdgeP2.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection)) {
+                                            leftEdges.add(it)
+                                            rightEdges.add(rightEdgeP2)
                                         }
-                                        leftEdgeP2?.isIntersectedBy(it) != null -> {
-                                            if (leftEdgeP2.isPointInside(leftEdgeP1Top)) leftEdges.add(leftEdgeP2) else leftEdges.add(it)
-                                        }
+                                    }
+                                    if (leftEdgeP2?.isIntersectedBy(it) != null) {
+                                        val intersection = leftEdgeP2.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection))
+                                            if (leftEdgeP2.isPointInside(leftEdgeP1Top!!)) leftEdges.add(leftEdgeP2) else leftEdges.add(it)
                                     }
                                 }
                                 // Working with Right of C1
                                 rightEdgeP1 -> rightEdgeP1.also {
-                                    when {
-                                        isPointInsideP2(rightEdgeP1Top!!, leftEdgeP2, rightEdgeP2) -> rightEdges.add(it)
-                                        leftEdgeP2 != null && it.isIntersectedBy(leftEdgeP2) != null -> {
-                                            if (!leftEdgeP2.isPointInside(rightEdgeP1Top)) {
-                                                leftEdges.add(leftEdgeP2)
-                                                rightEdges.add(it)
-                                            }
+
+                                    if (isPointInsideP2(rightEdgeP1Top!!, leftEdgeP2, rightEdgeP2)) rightEdges.add(it)
+                                    if (leftEdgeP2?.isIntersectedBy(it) != null && !leftEdgeP2.isPointInside(rightEdgeP1Top!!)) {
+                                        val intersection = leftEdgeP2.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection)) {
+                                            leftEdges.add(leftEdgeP2)
+                                            rightEdges.add(it)
                                         }
-                                        rightEdgeP2?.isIntersectedBy(it) != null -> {
-                                            if (rightEdgeP2.isPointInside(rightEdgeP1Top)) rightEdges.add(rightEdgeP2) else rightEdges.add(it)
-                                        }
+                                    }
+                                    if (rightEdgeP2?.isIntersectedBy(it) != null) {
+                                        val intersection = rightEdgeP2.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection))
+                                            if (rightEdgeP2.isPointInside(rightEdgeP1Top!!)) rightEdges.add(rightEdgeP2) else rightEdges.add(it)
                                     }
                                 }
                                 // Working with Left of C2
                                 leftEdgeP2 -> leftEdgeP2.also {
-                                    when {
-                                        isPointInsideP1(leftEdgeP2Top!!, leftEdgeP1, rightEdgeP1) -> leftEdges.add(it)
-                                        rightEdgeP1 != null && it.isIntersectedBy(rightEdgeP1) != null -> {
-                                            if (!rightEdgeP1.isPointInside(leftEdgeP2Top)) {
-                                                leftEdges.add(it)
-                                                rightEdges.add(rightEdgeP1)
-                                            }
+                                    if (isPointInsideP1(leftEdgeP2Top!!, leftEdgeP1, rightEdgeP1)) leftEdges.add(it)
+                                    if (rightEdgeP1?.isIntersectedBy(it) != null && !rightEdgeP1.isPointInside(leftEdgeP2Top!!)) {
+                                        val intersection = rightEdgeP1.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection)) {
+                                            leftEdges.add(it)
+                                            rightEdges.add(rightEdgeP1)
                                         }
-                                        leftEdgeP1?.isIntersectedBy(it) != null -> {
-                                            if (leftEdgeP1.isPointInside(leftEdgeP2Top)) leftEdges.add(leftEdgeP1) else leftEdges.add(it)
-                                        }
+                                    }
+                                    if (leftEdgeP1?.isIntersectedBy(it) != null) {
+                                        val intersection = leftEdgeP1.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection))
+                                            if (leftEdgeP1.isPointInside(leftEdgeP2Top!!)) leftEdges.add(leftEdgeP1) else leftEdges.add(it)
                                     }
                                 }
                                 // Working with Right of C2
                                 rightEdgeP2 -> rightEdgeP2.also {
-                                    when {
-                                        isPointInsideP1(rightEdgeP2Top!!, leftEdgeP1, rightEdgeP1) -> rightEdges.add(it)
-                                        leftEdgeP1 != null && it.isIntersectedBy(leftEdgeP1) != null -> {
-                                            if (!leftEdgeP1.isPointInside(rightEdgeP2Top)) {
-                                                leftEdges.add(leftEdgeP1)
-                                                rightEdges.add(it)
-                                            }
+                                    if (isPointInsideP1(rightEdgeP2Top!!, leftEdgeP1, rightEdgeP1)) rightEdges.add(it)
+                                    if (leftEdgeP1?.isIntersectedBy(it) != null && !leftEdgeP1.isPointInside(rightEdgeP2Top!!)) {
+                                        val intersection = leftEdgeP1.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection)) {
+                                            leftEdges.add(leftEdgeP1)
+                                            rightEdges.add(it)
                                         }
-                                        rightEdgeP1?.isIntersectedBy(it) != null -> {
-                                            if (rightEdgeP1.isPointInside(rightEdgeP2Top)) rightEdges.add(rightEdgeP1) else rightEdges.add(it)
-                                        }
+                                    }
+                                    if (rightEdgeP1?.isIntersectedBy(it) != null) {
+                                        val intersection = rightEdgeP1.isIntersectedBy(it)!!
+                                        if (currentEventHalfPlain.isPointInside(intersection))
+                                            if (rightEdgeP1.isPointInside(rightEdgeP2Top!!)) rightEdges.add(rightEdgeP1) else rightEdges.add(it)
                                     }
                                 }
                             }
                         }
+                        lastEventPoint = eventPoint
                     }
                     // Traverse event points of current status
                     eventPointQueue.clear()
-                    // TODO: move event point
-                    //      move pointer according on event point to one of bottom ends
                     leftEdgeP1Bottom = leftEdgeP1?.let { polygon1.findBottomCornerOfLeftEdge(it, leftEdgeP1Index) }
                     rightEdgeP1Bottom = rightEdgeP1?.let { polygon1.findBottomCornerOfRightEdge(it, rightEdgeP1Index) }
                     leftEdgeP2Bottom = leftEdgeP2?.let { polygon2.findBottomCornerOfLeftEdge(it, leftEdgeP2Index) }
                     rightEdgeP2Bottom = rightEdgeP2?.let { polygon2.findBottomCornerOfRightEdge(it, rightEdgeP2Index) }
-                    //                eventPointQueue.addAll()
-
-                    // TODO: move to proper handling procedures
-                    ++leftEdgeP1Index
-                    ++rightEdgeP1Index
-                    ++leftEdgeP2Index
-                    ++rightEdgeP2Index
+                    // TODO: null checks
+                    eventPointQueue.add(mutableListOf<Point>().apply {
+                        leftEdgeP1Bottom?.let { add(it) }
+                        rightEdgeP1Bottom?.let { add(it) }
+                        leftEdgeP2Bottom?.let { add(it) }
+                        rightEdgeP2Bottom?.let { add(it) }
+                    }.maxBy { it.y }!!)
+                    when (eventPointQueue[0].y) {
+                        leftEdgeP1Bottom?.y -> ++leftEdgeP1Index
+                        rightEdgeP1Bottom?.y -> ++rightEdgeP1Index
+                        leftEdgeP2Bottom?.y -> ++leftEdgeP2Index
+                        rightEdgeP2Bottom?.y -> ++rightEdgeP2Index
+                    }
                 } while (lastEventPoint.y != endEventPointY)
             }
         }
--- src/entities/Line.kt
+++ src/entities/Line.kt
@@ -1,5 +1,6 @@
 package entities
 
+import EPSILON
 import kotlin.math.*
 
 data class Line(val a: Double, val b: Double, val c: Double) {
@@ -8,24 +9,25 @@ data class Line(val a: Double, val b: Double, val c: Double) {
         return if ((a / b) / (line.a / line.b) == 1.0) {
             null
         } else {
-            val x = (line.c - c * line.b) / (a * line.b - b * line.a)
-            Point(x, -(c + a * x) / b)
+            val y = (line.a * c - a * line.c) / (a * line.b - b * line.a)
+            Point((-c - b * y) / a, y)
         }
     }
 
     /* Finds polar angle between (0,1) and (a, b) */
     fun getPolarAngle(): Double? {
         // TODO: use epsilon
-        val isLeft = (a > 0 && b >= Double.NEGATIVE_INFINITY && b < Double.POSITIVE_INFINITY) || (a == 0.0 && b < 0)
-        val isRight = (a < 0 && b > Double.NEGATIVE_INFINITY && b <= Double.POSITIVE_INFINITY) || (a == 0.0 && b > 0)
+        val isLeft = (a > 0 && b >= Double.NEGATIVE_INFINITY && b < Double.POSITIVE_INFINITY) || (a == 0.0 && b < 0.0)
+        val isRight = (a < 0 && b > Double.NEGATIVE_INFINITY && b <= Double.POSITIVE_INFINITY) || (a == 0.0 && b > 0.0)
         return when {
             isLeft -> acos(b / sqrt(a.pow(2) + b.pow(2)))
             isRight -> -acos(b / sqrt(a.pow(2) + b.pow(2)))
             else -> null // Not related to problem definition
         }
     }
+
     // TODO: use Epsilon
-    fun isPointInside(point: Point) = (a * point.x + b * point.y == -c)
+    fun isPointInside(point: Point) = (abs(a * point.x + b * point.y + c) <= EPSILON)
 
     companion object {
         @JvmStatic
